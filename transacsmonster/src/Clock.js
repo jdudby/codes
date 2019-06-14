daa8c7996b4184a07ae8f1b76b5c4c4ad0e1074e
Carl Jean-Pierre <cjean-pierre@haiti-psm.local>
2019-06-14 15:53:31 -0400

    Gestion des événements maitrisée

--- transacsmonster/src/Clock.js
+++ transacsmonster/src/Clock.js
@@ -20,7 +20,7 @@ class Clock extends React.Component{
   }
 
   componentWillUnmount(){
-  
+
   }
 
 
--- transacsmonster/src/Titre.js
+++ transacsmonster/src/Titre.js
@@ -1,12 +1,39 @@
 import React from "react";
 
-function Titre(){
+class Titre extends React.Component
+{
 
-return(<div>
-<h1>Transacs Monster</h1>
+constructor(props){
+super(props);
+this.state={ text:"Tu n'es pas cliqué !"};
+this.superClick=this.superClick.bind(this);
+this.state={compteur:0};
+}
+
+superClick(){
+  this.setState({text:"Tu es cliqué !"});
+}
+
+render(){
+
+return(<div onClick={this.superClick}>
+<h1>Transacs Monsters</h1>
+<p>{this.state.text}</p>
+<p>Rendering {this.state.compteur} fois ! </p>
 </div>
 );
 }
 
+componentDidMount(){
+setInterval(()=>this.setState({text:"Tu n'es pas cliqué fonction interval!", compteur:this.state.compteur+1}), 5000);
+}
+
+componentWillUnmount(){
+
+}
+
+
+
+}
 
 export default Titre;
